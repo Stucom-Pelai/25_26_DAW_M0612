ab48cc580dda9afb3970009b0b1f44349d067c9e
cristiancat <cristian.catalan@stucom.com>
2025-12-18 17:38:44 +0100

    exemple exempleModuls

--- pr04/exempleClasses.js
+++ pr04/exempleClasses.js
@@ -1,9 +1,11 @@
+import {ClasseNova} from  "./exempleModuls.js" 
+
 let miObjeto={titol:"Es mi objeto",
                 descripcio:"qué objeto más bonito"}
 miObjeto.lema="Los objetos molan mucho";
 miObjeto.mostraLema= function(){
     console.log("El lema és:"+miObjeto.lema)
-}
+}   
 
 console.log(miObjeto.titol)
 console.log(miObjeto.descripcio)
@@ -11,18 +13,7 @@ console.log(miObjeto.lema)
 miObjeto.mostraLema();
 console.log("####EXEMPLE CLASSES AMB FUNCTION###")
 
-function ClasseNova(){
-    this.titol="Titol per defecte"
-    this.lema=""
-    this.descripcio="encara no has indicat una descripció"
-    this.mostraLema=function(){
-        console.log("El lema és:"+this.lema)
-    }
-    lema ="just code it";
-    if(this.lema==""){
-        this.lema=this.titol;
-    }
-}
+
 let objecte2 = new ClasseNova()
 objecte2.lema="pop don't stop";
 objecte2.mostraLema();
--- pr04/exempleModuls.js
+++ pr04/exempleModuls.js
@@ -1,3 +1,16 @@
 function saluda(){
     console.log("Hola món des d'un mòdul!");
 }
+
+export function ClasseNova(){
+    this.titol="Titol per defecte"
+    this.lema=""
+    this.descripcio="encara no has indicat una descripció"
+    this.mostraLema=function(){
+        console.log("El lema és:"+this.lema)
+    }
+    // lema ="just code it";
+    if(this.lema==""){
+        this.lema=this.titol;
+    }
+}
